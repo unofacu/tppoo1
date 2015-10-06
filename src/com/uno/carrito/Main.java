3e28de295a856fde843ade5535ffa0ba0d9e8575
Carlos Bellotti <Carlos Bellotti>
2015-10-05 22:54:16 -0300

    Retoco clase ProductosAComprar

--- src/com/uno/carrito/Main.java
+++ src/com/uno/carrito/Main.java
@@ -5,36 +5,36 @@ public class Main {
 
 	public static void main(String[] args) {
 
-		ProductosAComprar lista = new ProductosAComprar();
+		ProductosAComprar lista = new ProductosAComprar("listaproductos.txt");
 			
-		Carrito carrito1 = new Carrito();
-		Carrito carrito2 = new Carrito();
-		Carrito carrito3 = new Carrito();
-		
-		Cliente cliente = new Cliente("Carlos Perez", "Rivadavia 620", "Rivadavia 620", "carlos@mail.com");
-		ClientePreferencial cliente2 = new ClientePreferencial("Carlos Perez", "Rivadavia 620", "Rivadavia 620", "carlos@mail.com", 20);
-		
-		cliente.agregarCarrito(carrito1);
-		cliente.agregarCarrito(carrito2);
-		cliente2.agregarCarrito(carrito3);
-		
-		cliente.getCarrito(0).agregarProducto(new Producto(1, "Atlas Argentina", 5.2f), 2);
-		cliente.getCarrito(0).agregarProducto(new Producto(2, "CD Metallica", 10.5f), 1);
-		cliente.getCarrito(0).agregarProducto(new Producto(1, "Atlas Argentina", 5.20f), 1);
-		cliente.getCarrito(0).agregarProducto(new Producto(3, "Libro de Cocina", 7.35f), 2);
-
-		cliente.getCarrito(1).agregarProducto(new Producto(1, "Pelota de Futbol", 15.20f), 2);
-		cliente.getCarrito(1).agregarProducto(new Producto(2, "Mouse", 17.8f), 1);
-				
-		cliente2.getCarrito(0).agregarProducto(new Producto(1, "Atlas Argentina", 5.20f), 2);
-		cliente2.getCarrito(0).agregarProducto(new Producto(2, "CD Metallica", 10.53f), 1);
-		cliente2.getCarrito(0).agregarProducto(new Producto(1, "Atlas Argentina", 5.20f), 1);
-		cliente2.getCarrito(0).agregarProducto(new Producto(3, "Libro de Cocina", 7.35f), 2);
-		
-		cliente.mostrarDetalle();
-		cliente2.mostrarDetalle();
+//		Carrito carrito1 = new Carrito();
+//		Carrito carrito2 = new Carrito();
+//		Carrito carrito3 = new Carrito();
+//		
+//		Cliente cliente = new Cliente("Carlos Perez", "Rivadavia 620", "Rivadavia 620", "carlos@mail.com");
+//		ClientePreferencial cliente2 = new ClientePreferencial("Carlos Perez", "Rivadavia 620", "Rivadavia 620", "carlos@mail.com", 20);
+//		
+//		cliente.agregarCarrito(carrito1);
+//		cliente.agregarCarrito(carrito2);
+//		cliente2.agregarCarrito(carrito3);
+//		
+//		cliente.getCarrito(0).agregarProducto(new Producto(1, "Atlas Argentina", 5.2f), 2);
+//		cliente.getCarrito(0).agregarProducto(new Producto(2, "CD Metallica", 10.5f), 1);
+//		cliente.getCarrito(0).agregarProducto(new Producto(1, "Atlas Argentina", 5.20f), 1);
+//		cliente.getCarrito(0).agregarProducto(new Producto(3, "Libro de Cocina", 7.35f), 2);
+//
+//		cliente.getCarrito(1).agregarProducto(new Producto(1, "Pelota de Futbol", 15.20f), 2);
+//		cliente.getCarrito(1).agregarProducto(new Producto(2, "Mouse", 17.8f), 1);
+//				
+//		cliente2.getCarrito(0).agregarProducto(new Producto(1, "Atlas Argentina", 5.20f), 2);
+//		cliente2.getCarrito(0).agregarProducto(new Producto(2, "CD Metallica", 10.53f), 1);
+//		cliente2.getCarrito(0).agregarProducto(new Producto(1, "Atlas Argentina", 5.20f), 1);
+//		cliente2.getCarrito(0).agregarProducto(new Producto(3, "Libro de Cocina", 7.35f), 2);
+//		
+//		cliente.mostrarDetalle();
+//		cliente2.mostrarDetalle();
 		//////////////////////////
-		lista.listaDeProductos();
+		lista.mostrarListaProductos();
 	
 	}
 	
--- src/com/uno/carrito/ProductosAComprar.java
+++ src/com/uno/carrito/ProductosAComprar.java
@@ -6,56 +6,61 @@ import java.io.IOException;
 import java.util.ArrayList;
 
 public class ProductosAComprar {
-		
-		protected static ArrayList <Producto> listaProductos = new ArrayList <Producto>();
-		Producto prodtemporal;
-		
-		public ProductosAComprar() {
-		}
-		
-		public void listaDeProductos (){
-			try {
-				FileReader input = new FileReader("listaproductos.txt");
-				BufferedReader bufInput = new BufferedReader(input);
-				
-				String line;
-				
-				line = bufInput.readLine();
-				
-				while(line != null){
-					String [] datos;
-					
-					datos = line.split(":");
-								
-					int id = Integer.parseInt(datos[0]);
-					String nombre  = (datos[1]);
-					float precio = Float.parseFloat(datos[2]);
-					
-					prodtemporal= new Producto(id, nombre, precio); 
-				    listaProductos.add(prodtemporal);
-				
-				line = bufInput.readLine();
-				
-				}
-				
-				bufInput.close();
-				
-				for (int i = 0; i < listaProductos.size(); i++) {//imprimo la lista
-					System.out.printf("\t%3d\t%-30s\t%3.2f\n", listaProductos.get(i).getId(), 
-							listaProductos.get(i).getNombre(), listaProductos.get(i).getPrecio());
 
-				}
-				
-			} 
-			 
-			catch (IOException e){
-				e.printStackTrace();
-		    }
-		}		
+	private ArrayList<Producto> listaProductos;
+	Producto prodtemporal;
 
-	public void mostrarCantidad() {
-		System.out.println(listaProductos.size());
+	public ProductosAComprar(String nombreArchivo) {
+
+		this.listaProductos = new ArrayList<Producto>();
+		this.cargarListaProductos(nombreArchivo);
+
 	}
-	
-}
 
+	public void cargarListaProductos(String nombreArchivo) {
+
+		try {
+			FileReader input = new FileReader(nombreArchivo);
+			BufferedReader bufInput = new BufferedReader(input);
+
+			String line;
+
+			line = bufInput.readLine();
+
+			while (line != null) {
+				String[] datos;
+
+				datos = line.split(":");
+
+				int id = Integer.parseInt(datos[0]);
+				String nombre = (datos[1]);
+				float precio = Float.parseFloat(datos[2]);
+
+				prodtemporal = new Producto(id, nombre, precio);
+				listaProductos.add(prodtemporal);
+
+				line = bufInput.readLine();
+
+			}
+
+			bufInput.close();
+
+		}
+
+		catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void mostrarListaProductos() {
+
+		for (int i = 0; i < listaProductos.size(); i++) {// imprimo la lista
+			System.out.printf("\t%3d\t%-30s\t%3.2f\n", listaProductos.get(i)
+					.getId(), listaProductos.get(i).getNombre(), listaProductos
+					.get(i).getPrecio());
+
+		}
+
+	}
+
+}
