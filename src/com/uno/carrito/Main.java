fc2c58784634683d93aa5543ef3b3b5fbc10efce
JMFERNANDEZ <JMFERNANDEZ@2NEC2103.Tmoviles.com.ar>
2015-10-07 21:38:40 -0300

    agrega clase tarjeta

--- src/com/uno/carrito/Main.java
+++ src/com/uno/carrito/Main.java
@@ -20,43 +20,54 @@ public class Main {
 		int cvs = 0;
 		int descuento = 0;
 		Carrito carrito;
-		
-		
-//		Carrito carrito2 = new Carrito();
-//		Carrito carrito3 = new Carrito();
-//		
-//		Cliente cliente = new Cliente("Carlos Perez", "Rivadavia 620", "Rivadavia 620", "carlos@mail.com");
-//		ClientePreferencial cliente = new ClientePreferencial("Carlos Perez", "Rivadavia 620", "Rivadavia 620", "carlos@mail.com", 20);
-//		
-//		cliente.agregarCarrito(carrito1);
-//		cliente.agregarCarrito(carrito2);
-//		cliente2.agregarCarrito(carrito3);
-//		
-//		cliente.getCarrito(0).agregarProducto(new Producto(1, "Atlas Argentina", 5.2f), 2);
-//		cliente.getCarrito(0).agregarProducto(new Producto(2, "CD Metallica", 10.5f), 1);
-//		cliente.getCarrito(0).agregarProducto(new Producto(1, "Atlas Argentina", 5.20f), 1);
-//		cliente.getCarrito(0).agregarProducto(new Producto(3, "Libro de Cocina", 7.35f), 2);
-//
-//		cliente.getCarrito(1).agregarProducto(new Producto(1, "Pelota de Futbol", 15.20f), 2);
-//		cliente.getCarrito(1).agregarProducto(new Producto(2, "Mouse", 17.8f), 1);
-//				
-//		cliente2.getCarrito(0).agregarProducto(new Producto(1, "Atlas Argentina", 5.20f), 2);
-//		cliente2.getCarrito(0).agregarProducto(new Producto(2, "CD Metallica", 10.53f), 1);
-//		cliente2.getCarrito(0).agregarProducto(new Producto(1, "Atlas Argentina", 5.20f), 1);
-//		cliente2.getCarrito(0).agregarProducto(new Producto(3, "Libro de Cocina", 7.35f), 2);
-//		
-//		cliente.mostrarDetalle();
-//		cliente2.mostrarDetalle();
+
+		// Carrito carrito2 = new Carrito();
+		// Carrito carrito3 = new Carrito();
+		//
+		// Cliente cliente = new Cliente("Carlos Perez", "Rivadavia 620",
+		// "Rivadavia 620", "carlos@mail.com");
+		// ClientePreferencial cliente = new ClientePreferencial("Carlos Perez",
+		// "Rivadavia 620", "Rivadavia 620", "carlos@mail.com", 20);
+		//
+		// cliente.agregarCarrito(carrito1);
+		// cliente.agregarCarrito(carrito2);
+		// cliente2.agregarCarrito(carrito3);
+		//
+		// cliente.getCarrito(0).agregarProducto(new Producto(1, "Atlas
+		// Argentina", 5.2f), 2);
+		// cliente.getCarrito(0).agregarProducto(new Producto(2, "CD Metallica",
+		// 10.5f), 1);
+		// cliente.getCarrito(0).agregarProducto(new Producto(1, "Atlas
+		// Argentina", 5.20f), 1);
+		// cliente.getCarrito(0).agregarProducto(new Producto(3, "Libro de
+		// Cocina", 7.35f), 2);
+		//
+		// cliente.getCarrito(1).agregarProducto(new Producto(1, "Pelota de
+		// Futbol", 15.20f), 2);
+		// cliente.getCarrito(1).agregarProducto(new Producto(2, "Mouse",
+		// 17.8f), 1);
+		//
+		// cliente2.getCarrito(0).agregarProducto(new Producto(1, "Atlas
+		// Argentina", 5.20f), 2);
+		// cliente2.getCarrito(0).agregarProducto(new Producto(2, "CD
+		// Metallica", 10.53f), 1);
+		// cliente2.getCarrito(0).agregarProducto(new Producto(1, "Atlas
+		// Argentina", 5.20f), 1);
+		// cliente2.getCarrito(0).agregarProducto(new Producto(3, "Libro de
+		// Cocina", 7.35f), 2);
+		//
+		// cliente.mostrarDetalle();
+		// cliente2.mostrarDetalle();
 		//////////////////////////
-		//lista.mostrarListaProductos();
-	
+		// lista.mostrarListaProductos();
+
 		Cliente cliente = null;
 		int carritoActual = 0;
 		Scanner keyboard = new Scanner(System.in);
 		int opcion = 0;
-				
-		while (!( opcion ==8 )) {
-			
+
+		while (!(opcion == 8)) {
+
 			System.out.println("1.-Ingresar cliente");
 			System.out.println("2.-Agregar Carrito al cliente.");
 			System.out.println("3.-Listar Carritos");
@@ -68,104 +79,106 @@ public class Main {
 			System.out.println("");
 			System.out.print("Ingrese una opcion: ");
 			opcion = keyboard.nextInt();
-			
-			
-			switch (opcion) {
-            case 1:
-            	
-            	System.out.println("Ingrse el nomrbre del cliente: ");
-            	nombre = keyboard.next();
-            	System.out.println("Ingrese la direccion de Envio: ");
-            	dirdeenvio = keyboard.next();
-            	System.out.println("Ingrese la direccion de Cobro: ");
-            	dirdecobro = keyboard.next();
-            	System.out.println("Ingrese el mail del cliente: ");
-            	mail = keyboard.next();
-            	
-            	// Solicitar datos de Tarjeta
-            	System.out.print("Ingrese nro. de Tarjeta: ");
-            	nroTarjeta = keyboard.nextLong();
-            	System.out.print("Ingrese valida desde (aaaa-mm-d): ");
-            	validoDesde = keyboard.next();
-            	System.out.print("Ingrese valida hasta (aaaa-mm-d): ");
-            	validoHasta = keyboard.next();
-            	System.out.print("Ingrese codigo de verificacion: ");
-            	cvs = keyboard.nextInt();
-            	
-            	Tarjeta tarjeta = new Tarjeta(nroTarjeta, validoDesde, validoHasta, cvs);
-            	System.out.print("Ingrese el procentaje de descuento: ");
-            	descuento = keyboard.nextInt();
-            	 			
-            				if ((descuento > 0) || (descuento <= 50)){
-            					
-            					cliente = new ClientePreferencial(nombre, dirdeenvio, dirdecobro, mail, tarjeta, descuento);
-            				}
-            				else 
-            					cliente = new Cliente (nombre, dirdeenvio, dirdecobro, mail, tarjeta);
-            				
-            		break;
-            case 2:
-            		cliente.agregarCarrito(new Carrito());
-            		System.out.print("Su nuevo Carrito fue creado!!!! presione enter para continuar...");
-            		System.in.read();
-            		
-            		break;
-            case 3:
-            		cliente.ListarCarritos();
-            	 	break;
-            case 4: 
-            		System.out.print("Ingrese el carrito a usar: ");
-            		carritoActual = keyboard.nextInt();
-            		break;
-            case 5:
-            		lista.mostrarListaProductos();
-            		break;
-            case 6:
-            		System.out.print("Ingrese el ID del producto a comprar: ");
-            		int id = keyboard.nextInt();
-            		System.out.print("Ingrese la cantidad de unidades: ");
-            		int cantidad = keyboard.nextInt();
-            		
-            		Producto producto = lista.getProducto(id);
-            		if (producto == null)
-            			System.out.println("Producto inexistente.");
-            		else {
-            			System.out.printf("Se agregaron %d unidades del producto %d - %s. Total : %7.2f", cantidad, producto.getId(), producto.getNombre(), cantidad * producto.getPrecio());
-	            		cliente.getCarrito(carritoActual).agregarProducto(producto, cantidad);
-	            		System.out.printf("Su producto ha sido agregado al carrito Nro: %d\n", carritoActual);
-	            		System.out.println(" presione enter para continuar...");
-	            		System.in.read();
-}
-            		
-            		break;
-            case 7:
-            		cliente.mostrarDetalle();
-            		break;
-            case 8:
-            		System.out.println("Saliendo del programa!!!");
-            		break;
-            default:
-            		System.out.printf("Ingrese una opcion valida!!!\n\n\n");
-            		break;
-			
-		}
-		
-	} 
-}}
-	
-	// Metodo para pedir datos del cliente por pantalla y con los mismos crear una instancia de Cliente, si se ingresa un porcentaje de 
-	// descuento la instancia se hara con la clase ClientePreferencial. Esta opcion solo permitira cargar los datos del cliente si el objeto cliente == null
-	
-	// Metodo que agregue un carrito vacio al cliente
-	
-	// Motodo que muestra una lista de los carritos con su respectivo id (el id del carrito es igual a su posicion en el Arraylist de carritos del cliente)
-	
-	// Metodo para seleccionar carrito, mostrar la lista de carritos y el que selecciones lo carga en la variable carritoActual
-	
-	// Motodo para listar los productos disponibles. llama al metodo mostrarListaProductos() de la clase ProductosAComprar.
-	
-	// Metodo que solicita id de producto y cantidad, luego se llama al metodo agregarProducto del carrito apuntado por la variable carritoActual.
-	
-	
-	
 
+			switch (opcion) {
+			case 1:
+
+				System.out.println("Ingrse el nomrbre del cliente: ");
+				nombre = keyboard.next();
+				System.out.println("Ingrese la direccion de Envio: ");
+				dirdeenvio = keyboard.next();
+				System.out.println("Ingrese la direccion de Cobro: ");
+				dirdecobro = keyboard.next();
+				System.out.println("Ingrese el mail del cliente: ");
+				mail = keyboard.next();
+
+				// Solicitar datos de Tarjeta
+				System.out.print("Ingrese nro. de Tarjeta: ");
+				nroTarjeta = keyboard.nextLong();
+				System.out.print("Ingrese valida desde (aaaa-mm-d): ");
+				validoDesde = keyboard.next();
+				System.out.print("Ingrese valida hasta (aaaa-mm-d): ");
+				validoHasta = keyboard.next();
+				System.out.print("Ingrese codigo de verificacion: ");
+				cvs = keyboard.nextInt();
+
+				Tarjeta tarjeta = new Tarjeta(nroTarjeta, validoDesde, validoHasta, cvs);
+				System.out.print("Ingrese el procentaje de descuento: ");
+				descuento = keyboard.nextInt();
+
+				if ((descuento > 0) || (descuento <= 50)) {
+
+					cliente = new ClientePreferencial(nombre, dirdeenvio, dirdecobro, mail, tarjeta, descuento);
+				} else
+					cliente = new Cliente(nombre, dirdeenvio, dirdecobro, mail, tarjeta);
+
+				break;
+			case 2:
+				cliente.agregarCarrito(new Carrito());
+				System.out.print("Su nuevo Carrito fue creado!!!! presione enter para continuar...");
+				System.in.read();
+
+				break;
+			case 3:
+				cliente.ListarCarritos();
+				break;
+			case 4:
+				System.out.print("Ingrese el carrito a usar: ");
+				carritoActual = keyboard.nextInt();
+				break;
+			case 5:
+				lista.mostrarListaProductos();
+				break;
+			case 6:
+				System.out.print("Ingrese el ID del producto a comprar: ");
+				int id = keyboard.nextInt();
+				System.out.print("Ingrese la cantidad de unidades: ");
+				int cantidad = keyboard.nextInt();
+
+				Producto producto = lista.getProducto(id);
+				if (producto == null)
+					System.out.println("Producto inexistente.");
+				else {
+					System.out.printf("Se agregaron %d unidades del producto %d - %s. Total : %7.2f", cantidad,
+							producto.getId(), producto.getNombre(), cantidad * producto.getPrecio());
+					cliente.getCarrito(carritoActual).agregarProducto(producto, cantidad);
+					System.out.printf("Su producto ha sido agregado al carrito Nro: %d\n", carritoActual);
+					System.out.println(" presione enter para continuar...");
+					System.in.read();
+				}
+
+				break;
+			case 7:
+				cliente.mostrarDetalle();
+				break;
+			case 8:
+				System.out.println("Saliendo del programa!!!");
+				break;
+			default:
+				System.out.printf("Ingrese una opcion valida!!!\n\n\n");
+				break;
+
+			}
+
+		}
+	}
+}
+
+// Metodo para pedir datos del cliente por pantalla y con los mismos crear una
+// instancia de Cliente, si se ingresa un porcentaje de
+// descuento la instancia se hara con la clase ClientePreferencial. Esta opcion
+// solo permitira cargar los datos del cliente si el objeto cliente == null
+
+// Metodo que agregue un carrito vacio al cliente
+
+// Motodo que muestra una lista de los carritos con su respectivo id (el id del
+// carrito es igual a su posicion en el Arraylist de carritos del cliente)
+
+// Metodo para seleccionar carrito, mostrar la lista de carritos y el que
+// selecciones lo carga en la variable carritoActual
+
+// Motodo para listar los productos disponibles. llama al metodo
+// mostrarListaProductos() de la clase ProductosAComprar.
+
+// Metodo que solicita id de producto y cantidad, luego se llama al metodo
+// agregarProducto del carrito apuntado por la variable carritoActual.
---
+++ src/com/uno/carrito/Tarjeta.java
@@ -0,0 +1,66 @@
+package com.uno.carrito;
+
+import java.util.Date;
+
+public class Tarjeta {
+	
+	private long numero;
+	private String validaDesde;
+	private String validaHasta;
+	private int cvs;
+	
+
+	public Tarjeta(long numero, String validaDesde, String validaHasta, int cvs) {
+		super();
+		this.numero = numero;
+		this.validaDesde = validaDesde;
+		this.validaHasta = validaHasta;
+		this.cvs = cvs;
+	}
+
+
+	public long getNumero() {
+		return numero;
+	}
+
+
+	public void setNumero(long numero) {
+		this.numero = numero;
+	}
+
+
+	public String getValidaDesde() {
+		return validaDesde;
+	}
+
+
+	public void setValidaDesde(String validaDesde) {
+		this.validaDesde = validaDesde;
+	}
+
+
+	public String getValidaHasta() {
+		return validaHasta;
+	}
+
+
+	public void setValidaHasta(String validaHasta) {
+		this.validaHasta = validaHasta;
+	}
+
+
+	public int getCvs() {
+		return cvs;
+	}
+
+
+	public void setCvs(int cvs) {
+		this.cvs = cvs;
+	}
+
+
+	public boolean validar(int cvs) {
+		return (this.cvs == cvs);
+	}
+
+}
